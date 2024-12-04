f166953029f5bcd2c95475a00e101105d1046489
Saurabh <saurabh.d2106@gmail.com>
2024-12-04 13:08:36 +0000

    Day 3 code

--- src/test/java/utils/DataProviders.java
+++ src/test/java/utils/DataProviders.java
@@ -4,12 +4,10 @@ import org.testng.annotations.DataProvider;
 
 public class DataProviders {
 
-
     @DataProvider
-    public String[][] searchProductData(){
+    public String[][] searchProductData() {
 
         String[][] data = new String[3][2];
-
 
         data[0][0] = "Apple Watch";
         data[0][1] = "Electronics";
@@ -20,10 +18,31 @@ public class DataProviders {
         data[2][0] = "Apple Watch";
         data[2][1] = "Deals";
 
-      
         return data;
 
+    }
+
+    @DataProvider
+    public String[][] searchProductDataFromExcel() {
+
+
+        // 1. Open the workbook
+
+        // 2. Get the sheet
+
+        // 3. Get the row count
+
+        // 4. Get the cell count
+
+        // 5. Get the data
+
+        String[][] data = new String[3][2];
+
+
+
+        return data;
 
     }
-    
+
+
 }
